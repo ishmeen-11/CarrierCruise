a2d7e3144e3dd3bf9d75114a99c6349d8e916b5a
Ishmeen Kaur Garewal <145673226+ishmeen-11@users.noreply.github.com>
2023-12-11 23:47:41 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,6 @@
+const logoutBtn = document.getElementById('logout-btn');
+
+logoutBtn.addEventListener('click', () => {
+    // Implement logout functionality here
+    alert('You are logged out');
+});
